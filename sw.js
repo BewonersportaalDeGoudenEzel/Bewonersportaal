05ba1e0ed2e1ee23eb1dc7932a49439459086c4b
BewonersportaalDeGoudenEzel <polanenflat@outlook.com>
2026-09-11 00:25:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAAM = "gouden-ezel-v88";
+const CACHE_NAAM = "gouden-ezel-v91";
 const BESTANDEN = [
   "./",
   "./index.html",
@@ -6,6 +6,7 @@ const BESTANDEN = [
   "./vraag.html",
   "./zaal.html",
   "./mededeling-plaatsen.html",
+  "./dashboard.html",
   "./privacyverklaring.html",
   "./manifest.json",
   "./schoonmaakschema.js",
